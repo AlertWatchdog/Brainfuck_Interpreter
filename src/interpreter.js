165e922dc40f78196a01ca0bc2aa628f3c43a9e9
David Tenten <tenten.david00@gmail.com>
2020-07-06 17:59:43 +0200

    A lot

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,3 +1,72 @@
-function test (){
-    console.log('############')
+var arr = [];
+var loop = [];
+let pointer = 0;
+var i = 0;
+
+async function interpret(){
+    pointer = 0;
+    arr = [];
+    loop = [];
+    arr.push(0);  
+    i = 0;
+    exec(); 
 }
+
+function exec(){
+    let sourcecode = document.getElementById("scInput").value; 
+    for(; i < sourcecode.length; i++){
+        let c = sourcecode[i];
+        if(c === '+')
+            arr[pointer]++;
+        if(c === '-')
+            arr[pointer]--;
+        if(c === '.')
+                asciiOut(arr[pointer]);
+        if(c === '<')
+                pointerDec();
+        if(c === '>')
+                pointerInc();
+        if(c === '[')
+                loop.push(i)
+        if(c === ']'){
+            if(arr[pointer] !== 0){
+                i = loop[loop.length-1];
+            } else {
+                loop.pop();
+            }
+        }
+        if(c === ','){
+            i++;
+            break;
+        }
+        console.log(arr);
+    }        
+}
+
+function resumeExec(){
+    if(i !== 0){
+        arr[pointer] = document.getElementById("terminal").value.charCodeAt(document.getElementById("terminal").value.length-1);
+        exec();
+    }
+}
+
+function pointerInc(){
+    pointer++;
+    if(pointer === arr.length){
+        arr.push(0);
+    }
+    
+}
+
+function pointerDec(){
+    pointer = pointer - 1;
+    if(pointer < 0){
+        alert("Pointer exceeds Array-Limits (<0)")
+    }
+}
+
+function asciiOut(c){    
+    let output = document.getElementById("terminal")
+    output.value = output.value + String.fromCharCode(c);
+}
+
